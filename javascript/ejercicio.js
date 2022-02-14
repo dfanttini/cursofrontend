c251d6eb4a505d74832539cb33a8b51508b29433
dfanttini <fanttini@hotmail.com>
2022-02-14 11:34:01 +0100

    lunes temprano

---
+++ javascript/ejercicio.js
@@ -0,0 +1,12 @@
+
+// let a;
+// let b;
+// a=5;
+// b=10;
+// let=30;
+// let total= a + b;
+// console.log("total" + total);
+
+let im = I\'am = I am;
+let you = You\'re = you are;
+window.alert( im  + \"n" + you );
---
+++ javascript/ejercicio2.js
@@ -0,0 +1,3 @@
+let im= 'Iam = I am';
+let you= 'Youre = you are';
+prompt(im  + \"n" + you );
