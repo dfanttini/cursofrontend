dbcb51ce1d6c6c8ea590a95806d24965d8abf458
dfanttini <fanttini@hotmail.com>
2022-02-16 12:21:55 +0100

    miercoles 2

--- javascript/ejercicio.js
+++ javascript/ejercicio.js
@@ -7,6 +7,14 @@
 // let total= a + b;
 // console.log("total" + total);
 
-let im = I\'am = I am;
-let you = You\'re = you are;
-window.alert( im  + \"n" + you );
+// let im = I\'am = I am;
+// let you = You\'re = you are;
+// window.alert( im  + \"n" + you );
+let dato= "1234";
+let valoresAceptados= /^[0-9]+$/;
+if (dato.match(valoresAceptados)){
+    alert ("Es numérico");
+        }
+        else{
+         ("no es númerico");
+        }
