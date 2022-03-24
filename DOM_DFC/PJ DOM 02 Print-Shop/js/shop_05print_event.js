d88c63e6bda6a9c92974b6ecd5647d3135648b09
dfanttini <fanttini@hotmail.com>
2022-03-24 13:45:44 +0100

    jueves tarde

--- DOM_DFC/PJ DOM 02 Print-Shop/js/shop_05print_event.js
+++ DOM_DFC/PJ DOM 02 Print-Shop/js/shop_05print_event.js
@@ -1,41 +1,29 @@
-// Ara, anem a printar el carrito
+// Ara, anem a afegir els events en el codi maquetat
+// Aprenentges:
+//  Per reconeixer l'event, e.target.classList.contains('cardAdd'))
+//  pq fem servir el e.stopPropagation()
 
+const cards = document.getElementById('cards')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
-const templateCarrito = document.getElementById('template-carrito').content
-
 
 document.addEventListener('DOMContentLoaded', () => {
-    pintarCardsProducts(products);
+    pintarCardsProducts();
 });
 
-
-//FEM CLICK A CARDS
-const cards = document.getElementById('cards')
-cards.addEventListener('click', e => {
-
-    if (e.target.classList.contains('cardAdd')) {
+cards.addEventListener('click', e => {   
+    if (e.target.classList.contains('cardAdd')) {       
         addCarrito(e.target.id);
-        //setCarrito(e.target.parentElement)
     }
-    if (e.target.classList.contains('cardRemove')) {
+    if (e.target.classList.contains('cardRemove')) {       
         removeFromCart(e.target.id);
     }
-
     e.stopPropagation()
 });
 
-
-
-
-
-
-
-
 // Pintar tots els productes
-function pintarCardsProducts(listProducts) {
-
-    listProducts.forEach(item => {
+function pintarCardsProducts() {
+    products.forEach(item => {     
         templateCard.querySelector('.cardAdd').setAttribute('id', item.id)
         templateCard.querySelector('.cardRemove').setAttribute('id', item.id)
         templateCard.querySelector('img').setAttribute('src', item.img);
@@ -49,30 +37,15 @@ function pintarCardsProducts(listProducts) {
     cards.appendChild(fragment)
 }
 
-function calculateSubtotals() {
-    // 1. Create a for loop on the "cart" array 
-    // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
 
-    // calculem els subtotals depenent del tipus de compra
-    tipus = Object.keys(subtotal);
-    for (var i = 0; i < cart.length; i++) {
-        tipus.forEach(element => {
-            if (cart[i].type == element) {
-                total2 = subtotal[element].value + cart[i].price;
-                subtotal[element].value = parseFloat(total2.toFixed(2));
-                console.table(subtotal);
-            }
-        });
-    }
-}
 
 function calculateTotal() {
-    total=0;
     // Calculate total price of the cart either using the "cart" array
     for (var i = 0; i < cart.length; i++) {
-        total = total + cart[i].subtotal
+        total = total + cart[i].price
     }
-    return total.toFixed(2);
+    console.log("Total:" + total);
+    console.log("\n");
 }
 
 
@@ -87,45 +60,28 @@ function applyPromotionsCart(id) {
 
     /* Si l'usuari compra 3 o més ampolles d'oli (id:1), el preu del producte descendeix a 10 euros. */
     if ((cart[id].quantity >= 3) && (cart[id].id == 1)) {
-
-        let objIndex = products.findIndex((obj => obj.id == cart[id].id));
-        cart[id].subtotalWithDiscount = cart[id].quantity * products[cart[id].id].priceDiscount;
+        cart[id].subtotalWithDiscount = cart[id].quantity * 10;
         console.log("ha comprat 3 o més ampolles d'oli. Preu total amb descompte:" + cart[id].subtotalWithDiscount);
         console.log("\n");
     }
     /* En comprar-se 10 o més mescles per a fer pastís, el seu preu es rebaixa a 2/3. */
-    if ((cart[id].quantity >= 10) && (cart[id].id == 3)) {        
+    if ((cart[id].quantity >= 10) && (cart[id].id == 3)) {
         cart[id].subtotalWithDiscount = cart[id].quantity * ((cart[id].price) * 2) / 3;
         console.log("ha comprat 10 o més mescles de pastis. Preu total amb descompte:" + cart[id].subtotalWithDiscount);
         console.log("\n");
     }
 }
 
-const setCarrito = item => {    
-    const producto = {
-        title: item.querySelector('h5').textContent,
-        precio: item.querySelector('p').textContent,
-        id: item.querySelector('button').dataset.id,
-        cantidad: 1
-    }
-    // console.log(producto)
-    if (carrito.hasOwnProperty(producto.id)) {
-        producto.cantidad = carrito[producto.id].cantidad + 1
-    }
-
-    carrito[producto.id] = {
-        ...producto
-    }
-    pintarCarrito()
-}
 
 function addCarrito(id) {
+    // Refactor previous code in order to simplify it 
+    // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
     console.table(cart);
-    // El findIndex el fem servir pq ens torni l'id del objecte trobat.  
+    // El findIndex el fem servir pq ens torni l'id del objecte trobat. No ho he sabut fer amb el vanilla javascript 
     // Amb findIndex busquem totes les vegades que ho troba dins l'array
-
     let objIndex = cart.findIndex((obj => obj.id == products[id - 1].id));
+
     if (objIndex == -1) {
         //Si no està a la cart, l'afegim
         // no podem agafar la i, pq sinó se'ns desborda .. agafem l'ultim element.
@@ -133,52 +89,30 @@ function addCarrito(id) {
         cart[cart.length - 1].quantity = 1;
         cart[cart.length - 1].subtotal = products[id - 1].price;
         console.log("Afegit nou producte " + products[id - 1].name);
+        alert("Afegit nou producte " + products[id - 1].name)
     } else {
         // Si està a la cart, actualitzem
         cart[objIndex].quantity = cart[objIndex].quantity + 1;
-        //OJO, estem arrodonint a dos decimals             
-        cart[objIndex].subtotal = parseFloat((cart[objIndex].subtotal + cart[objIndex].price).toFixed(2));
+        cart[objIndex].subtotal = cart[objIndex].subtotal + cart[objIndex].price;
         console.log("Modificat producte de la llista " + cart[objIndex].name);
+        alert("Modificat producte de la llista " + cart[objIndex].name)
     }
-
 
     calculateSubtotals();
     calculateTotal();
     if (objIndex != -1) applyPromotionsCart(objIndex);
-
-    // cridem pintar carrito
-    pintarCarrito();
 }
 
 
 function removeFromCart(id) {
-    // Estic passant com a paràmetre el producte 1
+    // Estic passant com a paràmtre el producte 1
     // Busquem si el producte està a la cart
     let objIndex = cart.findIndex((obj => obj.id == id));
     if (objIndex != -1) {
         cart.splice(objIndex, 1);
-        console.log("Eliminat producte de la cart: " );
+        console.log("Eliminat producte de la cart: " + products[id - 1].name);
+        alert("Eliminat producte de la cart: " + products[id - 1].name)
     } else {
         alert("Aquest element no el tens al carret")
     }
-
-    // cridem pintar carrito
-    pintarCarrito();
 }
-
-function printCartModal(){
-    const items = document.getElementById('items');
-    items.innerHTML = ''; //esta vacio para que lo sume 
-    
-    cart.forEach(item =>{
-        templateCarrito.querySelector('th').textContent = item.id
-        templateCarrito.querySelector('td')[0].textContent = item.name
-        templateCarrito.querySelector('td')[1].textContent = item.price
-        templateCarrito.querySelector('td')[2].textContent = item.quantity
-        templateCarrito.querySelector('span').textContent = element.subtotal
-        const clone = templateCarrito.cloneNode(true)
-        fragment.appendChild(clone)
-    })
-}
-
-items.Modal.appendChild(fragment)
