8f1f8bcdafec4681d1677fcca68150bfb8e0f352
dfanttini <fanttini@hotmail.com>
2022-03-31 09:40:52 +0200

    jueves

---
+++ vue/app2.js
@@ -0,0 +1,14 @@
+const app = new Vue ({
+    el: "#app",
+    template: "#ejemplo2",
+    data:{
+        mensaje: "Este es un nuevo vue",
+        mensajeHTML: "<b>Esto es un mensaje mensajeHTML</b>",
+        imagen: "img/vue.png",
+        url: "http://www.uoc.edu",
+        number: 10,
+        ok: true,
+
+    }
+    
+})
---
+++ vue/app3.js
@@ -0,0 +1,10 @@
+const app = new Vue ({
+    el: "#app",
+    template: "#met",
+    data: {
+        mensaje: "asi es como se hace un metodo",
+        imagen: "img/vue.png",
+        encuesta: "img/encuesta.png",
+        url: "http:www.google.com",
+    }
+})
